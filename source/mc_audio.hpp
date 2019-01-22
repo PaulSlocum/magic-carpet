b9fca68adf94db32839d7bbfe64b9451f2134ddf
Paul Slocum <paul.slocum@gmail.com>
2019-01-22 13:17:33 -0800

    Added example files and attempted to set up Android example project

---
+++ source/mc_audio.hpp
@@ -0,0 +1,15 @@
+// mc_audio.hpp
+/////////////////////////////////////////////////////////////////////
+
+
+class MCState;
+
+class MCAudio 
+{
+public:
+    MCAudio();
+    ~MCAudio();
+    
+private:
+    MCState* state;
+};
